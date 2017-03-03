a91d2a43035497f15b56bff99ccd112c116076f6
FrankCaraballo <frankelias001@gmail.com>
2017-03-03 21:03:11 +0900

    port handling

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var todocontroler = require('./controlers/todocontrolers');
 var app= express();
 
+app.set('port', (process.env.PORT || 5000));
+
 //set template engine
 app.set('view engine','ejs');
 
@@ -13,5 +15,9 @@ app.use(express.static('./public'));
 todocontroler(app);
 console.log("Controlers excecuted");
 // port listener
-app.listen(3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 console.log("Listening to port 3000");
